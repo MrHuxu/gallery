4b4488804eae11847632f75dd42fa5659a58b685
xhu <xhu@freewheel.tv>
2017-08-03 13:08:52 +0800

    make test component not shown in homepage

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,29 +4,11 @@ import Menu from './menu';
 import PhotoGrid from './photo-grid';
 import SongList from './song-list';
 
-import DataBind from './data-bind';
-import Scale from './scale';
-import BarChart from './bar-chart';
-import FullBarChart from './full-bar-chart';
-import ChartAnimation from './chart-animation';
-import UpdateEnterExit from './update-enter-exit';
-import Interaction from './interaction';
-import PieLayout from './pie-layout';
-
 const App = () => (
   <div>
     <Menu />
     <PhotoGrid />
     <SongList />
-
-    <DataBind />
-    <Scale />
-    <BarChart />
-    <FullBarChart />
-    <ChartAnimation />
-    <UpdateEnterExit />
-    <Interaction />
-    <PieLayout />
   </div>
 );
 
